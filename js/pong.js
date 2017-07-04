488a045d9d12b21b1483ec9a60dd5132356094c8
Will Thurlow <willthurlow@yahoo.co.uk>
2017-07-04 18:16:49 +0100

    Initial set up; Added Index.html; added Pong.js

---
+++ js/pong.js
@@ -0,0 +1,31 @@
+/*
+Important Constants: Screen width, height, canvas, context, Paddle X, CPU X
+*/
+
+/*
+Variables:  Paddle Y, Ball X, Ball Y, Ball Speed, CPU Y, CPU Speed?, Player Score, CPU Score
+*/
+
+class Paddle{
+
+}
+class Ball{
+
+}
+init();
+/*
+1) Initialise the Ball
+2) Initialise the Paddles
+3) Initialise the Scores
+4) Initialise the update timer
+*/
+function init(){
+  
+}
+
+/*
+Game Loop: listen for user input and update all object info (each frame)
+*/
+function update(){
+
+}
